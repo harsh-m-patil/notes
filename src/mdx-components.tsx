a42042621928b83f074d9919c7a75c8821352d7b
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2025-09-02 21:55:39 +0530

    add cors and oops basic notes

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,10 +1,15 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
-import type { MDXComponents } from 'mdx/types';
+import defaultMdxComponents from "fumadocs-ui/mdx";
+import { Mermaid } from "@/components/mdx/mermaid";
+import * as TabsComponents from "fumadocs-ui/components/tabs";
+import * as AccordianComponents from "fumadocs-ui/components/accordion";
+import type { MDXComponents } from "mdx/types";
 
-// use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
-  return {
-    ...defaultMdxComponents,
-    ...components,
-  };
+	return {
+		...defaultMdxComponents,
+		Mermaid,
+		...TabsComponents,
+		...AccordianComponents,
+		...components,
+	};
 }
